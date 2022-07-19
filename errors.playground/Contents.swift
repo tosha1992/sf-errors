95b5b5638639e9797fe5f72105bab2d29c087552
tosha1992 <krimeainbox@yandex.ru>
2022-07-19 13:01:23 +0300

    Задание 3

--- errors.playground/Contents.swift
+++ errors.playground/Contents.swift
@@ -1,36 +1,15 @@
+// Задание 3
 
-// Задание 1.
-
-enum myErrors: Error {
-    case Error400
-    case Error404
-    case Error500
+func comparison  (_ valueA: Any, _ valueB: Any) -> Bool {
+    if type (of: valueA) == type (of: valueB) {
+        do {
+        print("Yes")
 }
-
-var Error400: Bool = false
-var Error404: Bool = false
-var Error500: Bool = true
-
-do {
-    if Error400 {
-        throw myErrors.Error400
+        return true;
     }
-    if Error404 {
-        throw myErrors.Error404
-    }
-    if Error500 {
-        throw myErrors.Error500
-    }
+    do {
+    print("No")
+return false
 }
-
-catch myErrors.Error400 {
-    print ("Error 400")
 }
-catch myErrors.Error404 {
-    print ("Error404")
-}
-catch myErrors.Error500 {
-    print("Error500")
-}
-
 
